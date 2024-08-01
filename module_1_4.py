8d2b66fe8a83d784d72eea1dad4901edd03906a3
Ferrum6400 <revolted6400@mail.ru>
2024-08-01 15:18:23 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+my_string = input()
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+first_char = my_string[0]
+last_char = my_string[-1]
+print(first_char)
+print(last_char)
+print(len(my_string))
